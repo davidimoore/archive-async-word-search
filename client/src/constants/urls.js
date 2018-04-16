c2546660514df6f7d2f32fb70b44661885305dd6
David Moore <davidimoore@gmail.com>
2018-04-15 23:28:31 -0700

    adds https to url

--- client/src/constants/urls.js
+++ client/src/constants/urls.js
@@ -1,3 +1,3 @@
 export const webstersSearchURL = `/api/webstersSearch`;
 export const urbanDictionarySearchUrl = `https://api.urbandictionary.com/v0/define`;
-export const thesaurusSearchUrl = `http://words.bighugelabs.com/api/2/${process.env.REACT_APP_BIG_HUGE_LABS_TOKEN}`;
+export const thesaurusSearchUrl = `https://words.bighugelabs.com/api/2/${process.env.REACT_APP_BIG_HUGE_LABS_TOKEN}`;
