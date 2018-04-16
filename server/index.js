64a20f39c52ea32c5446a2a27b057c597d586f49
David Moore <davidimoore@gmail.com>
2018-04-15 19:52:28 -0700

    update configs

--- server/index.js
+++ server/index.js
@@ -1,21 +1,45 @@
 const express = require('express');
-const cors = require('cors');
-const requests = require('../requests');
-const app = express();
-const port = process.env.PORT || 5000;
-app.use(cors());
-app.get("/hello", (req, res) => {
-    res.send({ hello: "world" })
-});
+const path = require('path');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+const requests = require('./requests');
 
-app.get("/api/webstersSearch/:word", (req, res) => {
-  const word = req.params.word;
-  requests.webstersSearch(word).then(response => {
-    res.json({ response })
-  })
-});
+const PORT = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+if (cluster.isMaster) {
+  console.error(`Node cluster master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+  });
+
+} else {
+  const app = express();
+  app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+  app.get("/api/webstersSearch/:word", (req, res) => {
+    const word = req.params.word;
+    requests.webstersSearch(word).then(response => {
+      res.json({ response })
+    })
+  });
+
+  app.get('*', function(request, response) {
+    response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+  });
+
+  app.listen(PORT, function () {
+    console.error(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
+  });
+
+}
+
+
 
 
 
